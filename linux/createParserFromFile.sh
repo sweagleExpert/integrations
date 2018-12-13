326a4fb6077c74b3ba07686b7748be1e3f8cac79
dfinas <dfinas@gmail.com>
2018-12-13 16:43:30 +0100

    create parser is able to update it if it is already there

--- linux/createParserFromFile.sh
+++ linux/createParserFromFile.sh
@@ -19,40 +19,143 @@ if [ "$#" -lt "2" ]; then
 fi
 argSourceFile=$1
 argParserType=$2
+parserId=""
 
 #filename without path and .js extension for import
 filename=$(basename "$argSourceFile" ".js")
 fileContent=`cat $argSourceFile`
 
-function apiUrl() {
-cat <<EOF
-$sweagleURL/api/v1/tenant/metadata-parser?name=$filename&description=$filename&parserType=$argParserType&errorDescriptionDraft=error+in+parser+$filename
-EOF
-}
-
-echo -e "\n**********"
-echo "*** Call Sweagle API to create parser: $filename"
-#echo "curl -s -X POST '$(apiUrl)'  --data-urlencode 'scriptDraft=$fileContent' -H '$(apiToken)')"
-response=$(curl -s -X POST "$(apiUrl)"  --data-urlencode "scriptDraft=$fileContent" -H "$(apiToken)")
-
-# Check if error returned because parser already exists
-if [[ $response == *"already exists"* ]]; then
-  echo "It's already there!"
-  # Build code here to extract ID of parser and update it
+#get description from firstline of file
+read -r firstline<$argSourceFile
+if [[ $firstline == "// description:"* ]]; then
+  description=$(echo $firstline| cut -d':' -f 2)
+else
+  description="$filename"
 fi
 
-parserID=$(echo "$response" | jsonValue "id")
+# Put json parsers list in variable parsersList
+function getParsers {
+  echo "*** Get parsers list"
+  response=$(curl -s -X GET "$sweagleURL/api/v1/tenant/metadata-parser" -H "Authorization: bearer $aToken" -H "Accept: application/vnd.siren+json")
+  # Check if any error before continue
+  errorFound=$(echo $response | jsonValue "error_description")
+  if [[ -z $errorFound ]]; then
+    parsersList="$response"
+  else
+    echo -e "\n**********"
+    echo "*** Error getting parser list: $errorFound"
+    exit 1
+  fi
+}
 
-if [[ ! -z $parserID ]]
-then
-  echo -e "\n**********"
-  echo "*** Creation successfull, call Sweagle API to publish parser id: $parserID"
-  response=$(curl -s -X POST "$sweagleURL/api/v1/tenant/metadata-parser/$parserID/publish" -H "$(apiToken)")
-  echo "Sweagle response:"
-  echo "$response"
+#Return Parser Id from Parsers list identified by its name
+function getParserIdFromName {
+parserName="$1" jsonValue="$2" python - <<EOF_PYTHON
+#!/usr/bin/python
+import json
+import os
+parserName = os.environ['parserName']
+#print("*** Use python to get Id for Parser "+parserName)
+json1 = json.loads(os.environ['jsonValue'])
+for item in json1["entities"]:
+  if item["properties"]["name"] == parserName:
+      print item["properties"]["id"]
+EOF_PYTHON
+}
+
+# Create parser from file and put new created Id in variable parserId
+function createParser {
+  local argName=$1
+  local argDescription=$2
+  local argScript=$3
+  echo "*** Create parser with name: $argName"
+  response=$(curl -s -X POST "$sweagleURL/api/v1/tenant/metadata-parser?name=$filename&parserType=$argParserType&errorDescriptionDraft=error+in+parser+$filename" --data-urlencode "description=$argDescription" --data-urlencode "scriptDraft=$argScript" -H "Authorization: bearer $aToken" -H "Accept: application/vnd.siren+json")
+  # Check if any error before continue
+  errorFound=$(echo $response | jsonValue "error_description")
+  if [[ -z $errorFound ]]; then
+    parserId=$(echo "$response" | jsonValue "id")
+    echo "*** Created parser with id: $parserId"
+  else
+    echo -e "\n**********"
+    echo "*** Error creating parser: $errorFound"
+  fi
+}
+
+# Update parser from Id + script
+function updateParser {
+  local argId=$1
+  local argDescription=$2
+  local argScript=$3
+  echo "*** Update parser $argId"
+  response=$(curl -s -X POST "$sweagleURL/api/v1/tenant/metadata-parser/$argId?"--data-urlencode "description=$argDescription" --data-urlencode "scriptDraft=$argScript" -H "Authorization: bearer $aToken" -H "Accept: application/vnd.siren+json")
+  # Check if any error before continue
+  errorFound=$(echo $response | jsonValue "error_description")
+  if [[ -z $errorFound ]]; then
+    echo "*** Updated parser with id: $argId"
+  else
+    echo -e "\n**********"
+    echo "*** Error updating parser: $errorFound"
+  fi
+}
+
+function publishParser {
+  local argId=$1
+  echo "*** Publish parser with id: $argId"
+  response=$(curl -s -X POST "$sweagleURL/api/v1/tenant/metadata-parser/$argId/publish" -H "Authorization: bearer $aToken" -H "Accept: application/vnd.siren+json" )
+  errorFound=$(echo $response | jsonValue "error_description")
+  if [[ -z $errorFound ]]; then
+    echo "*** Published parser $argId"
+  else
+    echo -e "\n**********"
+    echo "*** Error publishing parser: $errorFound"
+  fi
+}
+
+function getNextParserId {
+  echo "*** Get parser list"
+  response=$(curl -s -X GET -H "Authorization: bearer $aToken" -H "Accept: application/vnd.siren+json" "$sweagleURL/api/v1/tenant/metadata-parser")
+  # Check if any error before continue
+  errorFound=$(echo $response | jsonValue "error_description")
+  if [[ -z $errorFound ]]; then
+    # Extract next Parser Id from list
+    parserId=$(echo $response | jsonValue "id")
+    echo "Next parser Id is $parserId"
+  else
+    echo -e "\n**********"
+    echo "*** Error getting parser list: $errorFound"
+    exit 1
+  fi
+}
+
+function deleteParser {
+  local argId=$1
+  echo "*** Deleting parser $argId"
+  response=$(curl -s -X DELETE "$sweagleURL/api/v1/tenant/metadata-parser/$argId" -H "Authorization: bearer $aToken" -H "Accept: application/vnd.siren+json")
+  # Check if any error before continue
+  errorFound=$(echo $response | jsonValue "error_description")
+  if [[ -z $errorFound ]]; then
+    echo "*** Parser $argId deleted"
+  else
+    echo -e "\n**********"
+    echo "*** Error deleting parser: $errorFound"
+    exit 1
+  fi
+}
+
+
+echo -e "\n**********"
+createParser "$filename" "$description" "$fileContent"
+# Check if error returned because parser already exists
+if [[ $response == *"already exists"* ]]; then
+  echo "It's already there! Updating it"
+  getParsers
+  parserId=$(getParserIdFromName "$filename" "$parsersList")
+  updateParser $parserId "$description" "$fileContent"
+fi
+if [[ ! -z $parserId ]]; then
+  publishParser $parserId
 else
   echo -e "\n**********"
   echo "*** ERROR CREATING PARSER: $filename"
-  echo "$response"
   exit 1
 fi
