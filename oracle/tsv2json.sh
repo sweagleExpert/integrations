aff3d45588031996473e332d28f0e00d79bcd1d5
dimitris-sweagle <dimitris@sweagle.com>
2019-02-13 18:10:46 +0100

    remove useless comments

--- oracle/tsv2json.sh
+++ oracle/tsv2json.sh
@@ -4,11 +4,6 @@
 #############
 #############   TRANSFORM A TSV FILE AS JSON
 #############
-############# Input: 1- Path to upload to, defined by each of the node names separated by ,
-############# Input: 2- Config Directory or file to upload
-############# Input: 3- Optional: file extension to filter files to read
-############# supported extensions are json, xml, yaml, yml, others are considered as property files
-############# Output: 0 if no errors, 1 + Details of errors if any
 ##########################################################################
 if [ "$#" -lt "2" ]; then
     echo "********** ERROR: NOT ENOUGH ARGUMENTS SUPPLIED"
