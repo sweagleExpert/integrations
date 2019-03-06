61aa491a50ceb7382587f2f0ec5e1c2b0ec4242e
dfinas <dfinas@gmail.com>
2019-03-06 10:10:22 +0100

    more robust

--- pem/pem2ini.sh
+++ pem/pem2ini.sh
@@ -15,25 +15,32 @@ argDir=$1
 output=$2
 argFileExtension="pem"
 
-dir=$(dirname "${output}")
-mkdir -p $dir
-echo "" > $output
+# Check if input is a directory
+if [[ -d "$argDir" ]]; then
+  dir=$(dirname "${output}")
+  mkdir -p $dir
+  echo "" > $output
 
-cd "$argDir"
-for file in *.${argFileExtension}; do
-  filename=$(basename "${file%.*}")
-  # remove case where there is no result and "*" is returned
-  if [ "$filename" == "*" ]; then
-    echo "********** No file with extension: $argFileExtension"
-    echo "********** In directory: $argDir"
-    echo "********** Exiting without error"
-    exit 0
-  fi
-  echo "*** Store config of file: $filename"
-  echo "[$filename]" >> $output
-  echo file=$file  >> $output
-  echo $(openssl x509 -noout -issuer -in $file) >> $output
-  echo $(openssl x509 -noout -subject -in $file) >> $output
-  echo $(openssl x509 -enddate -noout -in $file) >> $output
-  echo >> $output
-done
+  for file in "$argDir"/*.${argFileExtension}; do
+    filename=$(basename "${file%.*}")
+    # remove case where there is no result and "*" is returned
+    if [ "$filename" == "*" ]; then
+      echo "********** No file with extension: $argFileExtension"
+      echo "********** In directory: $argDir"
+      echo "********** Exiting without error"
+      exit 0
+    fi
+    echo "*** Store config of file: $filename"
+    echo "[$filename]" >> $output
+    echo file=$file  >> $output
+    echo $(openssl x509 -noout -issuer -in $file) >> $output
+    echo $(openssl x509 -noout -subject -in $file) >> $output
+    echo "notAfter=$(date --date="$(openssl x509 -enddate -noout -in "$file"|cut -d= -f 2)" --iso-8601)" >> $output
+    #echo "notAfter=$(date --date="$(openssl x509 -enddate -noout -in "$file"|cut -d= -f 2)" --iso-8601=seconds)" >> $output
+    #echo $(openssl x509 -enddate -noout -in $file) >> $output
+    echo >> $output
+  done
+else
+  echo "********** Input $argDir is not a directory"
+  exit 1
+fi
