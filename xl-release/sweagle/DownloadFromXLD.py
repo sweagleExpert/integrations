34bc0ebbb58b8b437a71d526730bfea7ce8dbcfe
Dimitris FINAS <dfinas@gmail.com>
2019-03-22 18:17:31 +0100

    add comments, remove debugs

--- xl-release/sweagle/DownloadFromXLD.py
+++ xl-release/sweagle/DownloadFromXLD.py
@@ -1,6 +1,6 @@
 import requests
 from requests.auth import HTTPBasicAuth
-import java.net as jnet 
+import java.net as jnet
 import json
 from xml.dom import minidom
 
@@ -25,19 +25,18 @@ try:
 	#End if
 
 	api_uri_dic = uriprefix + "deployit/repository/ci/" + environment
-	
+
 	print ("\r *** Get list of Dictionaries from XLD")
 	try:
 		response = requestxld.get(api_uri_dic, body="", contentType='application/xml')
 	except jnet.UnknownHostException as ukhe:
-		raise Exception("get dictionaries error : " + str(ukhe) + "\n")
+		raise Exception("\r get dictionaries error : " + str(ukhe) + "\n")
 
 	if response.getStatus() != 200:
-		raise Exception("get dictionaries error. Status = [" + str(response.getStatus()) + "] Error : " + response.getResponse())
+		raise Exception("\r get dictionaries error. Status = [" + str(response.getStatus()) + "] Error : " + response.getResponse())
 	#End if
-    
-	#For DEBUG 
-	print ("\r - XLD response: " + response.getResponse())
+
+	#For DEBUG print ("\r - XLD response: " + response.getResponse())
 
 	xmldoc = minidom.parseString(response.getResponse())
 
@@ -51,24 +50,20 @@ try:
 
 	for lxlddictionary in lxlddictionaries:
 		lxldkeyvalues = dict()
-		
+
 		print ("\r *** Get Dictionary data from XLD")
 		api_uri_dictionary = uriprefix +  "deployit/repository/ci/" + lxlddictionary
 		try:
 			response = requestxld.get(api_uri_dictionary, body="", contentType='application/json')
 		except jnet.UnknownHostException as ukhe:
-			raise Exception("get dictionaries error : " + str(ukhe) + "\n")
+			raise Exception("\r get dictionaries error : " + str(ukhe) + "\n")
 
 		if response.getStatus() != 200:
-			raise Exception("get dictionaries error. Status = [" + str(response.getStatus()) + "] Error : " + response.getResponse())
+			raise Exception("\r get dictionaries error. Status = [" + str(response.getStatus()) + "] Error : " + response.getResponse())
 		#End if
 
-        #For DEBUG 
-		print("\r - Dictionary :" + response.getResponse())
-		
-		#data = {}
-		#data['type'] = 'udm.Dictionary'
-		#data['id'] = lxlddictionary
+        #For DEBUG print("\r - Dictionary :" + response.getResponse())
+
 		#xmldoc = minidom.parseString(response.getResponse())
 		#for resultItem in xmldoc.getElementsByTagName('entries').item(0).getElementsByTagName("entry"):
 		#	xldkey   = resultItem.getAttribute('key')
@@ -77,33 +72,39 @@ try:
 		#	else:
 		#		xldvalue = resultItem.firstChild.nodeValue
 		#	#Fin if
-		#	
-		#	
+		#
+		#
 		#	if xldvalue is None:
 		#		xldvalue = ""
 		#
 		#	lxldkeyvalues[xldkey] = xldvalue
 		#Fin for all items in Dictionnary
-		#data = json.loads(lxldkeyvalues)
+
+        # BEGIN: This is to use if you want SWEAGLE to contain only XLD dictionary key/values
+        #json_dict = json.loads(r.getResponse())
+        #data = json_dict['entries']
+        # END: This is to use if you want SWEAGLE to contain only XLD dictionary key/values
+        # BEGIN: This is to use if you want SWEAGLE to contain the full XLD dictionary object
 		data = response.getResponse()
-		
+        # END: This is to use if you want SWEAGLE to contain the full XLD dictionary object
+
 		print("\r - Send Dictionary " + lxlddictionary + "to SWEAGLE")
 		nodePath = lxlddictionary.replace("/", ",")
-		
+
 		headers = {"Authorization": "Bearer {0}".format(sweagleTenant['token']),
 			"Accept": "application/json",
 			"Content-Type": "application/json"}
-		
+
 		url = ("{0}/api/v1/data/bulk-operations/dataLoader/upload?"
 			"nodePath={1}&format=json&allowDelete=false&onlyParent=true"
 			"&autoApprove=true&storeSnapshotResults=false&validationLevel=warn"
 			"&encoding=utf-8".format(sweagleTenant['url'], nodePath))
-		
+
 		#For DEBUG print('\r * url: {0}'.format(url))
 		#For DEBUG print('\r * data: {0}'.format(data))
 		#For DEBUG print('\r * headers: {0}'.format(headers))
 		r = requests.post(url, headers=headers, data=data, verify=False)
-		
+
 		if r.status_code == requests.codes.ok:
 			response = r.json()
 			print("\r * SWEAGLE response: {}".format(json.dumps(response)))
