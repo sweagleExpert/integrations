0a30e5f20806222b0ec3724767546892f2abdcb2
dfinas <dfinas@gmail.com>
2019-03-27 18:28:31 +0100

    handle 3.0 API behavior for forIncoming=true

--- linux/checkCustomConfigByMdsValidator.sh
+++ linux/checkCustomConfigByMdsValidator.sh
@@ -17,26 +17,36 @@ fi
 argMds=$1
 argCustomValidator=$2
 argControl=result
+argError=error
 
 echo -e "\n**********"
 echo "*** Call Sweagle API to check configuration status for MDS: $argMds and VALIDATOR: $argCustomValidator"
 
 function apiUrl() {
 cat <<EOF
-$sweagleURL/api/v1/tenant/metadata-parser/validate?mds=$argMds&parser=$argCustomValidator&forIncoming=true
+$sweagleURL/api/v1/tenant/metadata-parser/validate?mds=$argMds&parser=$argCustomValidator
 EOF
 }
 
 # For debugging
-#echo "curl -s -X POST '$(apiUrl)' -H '$(apiToken)'"
-response=$(curl -s -X POST "$(apiUrl)" -H "$(apiToken)")
+#echo "curl -s -X POST '$(apiUrl)&forIncoming=true' -H '$(apiToken)'"
+# First, check result for pending MDS
+response=$(curl -s -X POST "$(apiUrl)&forIncoming=true" -H "$(apiToken)")
 echo "Response: " $response
+
+ec=$(echo "$response" | jsonValue $argError)
+if [ "$ec" = "NotFoundException" ]; then
+  echo "No pending MDS found, relaunch API to get last snapshot result instead"
+  response=$(curl -s -X POST "$(apiUrl)&forIncoming=false" -H "$(apiToken)")
+  echo "Response: " $response
+fi
+
 rc=$(echo "$response" | jsonValue $argControl)
 if [[ $response = "{\"error\":"* ]]; then
-  echo -e "\n********** ERROR: Unable to validate MDS: $argMds with VALIDATOR: $argCustomValidator \n"
-  exit 1
+    echo -e "\n********** ERROR: Unable to validate MDS: $argMds with validator: $argCustomValidator \n"
+    exit 1
 elif [ "$rc" = false ]; then
-    echo "********** ERROR: BROKEN configuration data detected for Validator: " $argCustomValidator
+    echo "********** ERROR: BROKEN configuration data detected for validator: " $argCustomValidator
     exit 1
 fi
 
