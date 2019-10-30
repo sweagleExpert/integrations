eef9d70df90311f9415eec0e496ac798a4be79f9
Dimitris FINAS <dfinas@gmail.com>
2019-10-30 14:35:11 +0100

    new jenkins org

---
+++ jenkins/jenkinsfile-directAPI.groovy
@@ -0,0 +1,219 @@
+import groovy.json.JsonSlurper
+
+def SWEAGLE_TENANT = "https://testing.sweagle.com"
+def SWEAGLE_TOKEN = "02eb161a-d8e6-43a6-883e-affcdd0b5975"
+
+def CONFIG_FILE = "./conf/toto.json"
+def CONFIG_FORMAT = "json"
+def SWEAGLE_PATH = "test-token"
+def SWEAGLE_MDS = "test-token"
+def SWEAGLE_VALIDATORS = "noEmptyValues noDevValue"
+def SWEAGLE_EXPORTER = "all"
+def SWEAGLE_EXPORTER_ARGS = ""
+def SWEAGLE_EXPORTER_FORMAT = "JSON"
+def SWEAGLE_EXPORTER_OUTPUT = "<OPTIONAL, TARGET FILE TO EXPORT CONFIG DATA TO, DEFAULT OUTPUT IS SCREEN> ex: /release/node1-conf.json"
+def SWEAGLE_SNAPSHOT_TAG = ""
+
+
+node {
+    //checkout scm
+
+    stage('Test API - CheckVersion') {
+      echo '######################################################'
+      echo '### Check Version'
+      def api = "/info"
+      def response = callSweagleAPI('GET',api)
+      println "API response: "+response
+    }
+
+    stage('UploadConfig') {
+      echo '######################################################'
+      echo '### Upload Configuration files to SWEAGLE'
+      def argDcsApprove="true"
+      def argDeleteData="false"
+      def argEncoding="utf-8"
+      def argIdentifierWords=""
+      def argOnlyParent="true"
+      def argSnapshotCreate="false"
+      def argSnapshotLevel="warn"
+      //String body = new File(CONFIG_FILE).text
+      def body = '{"id": 122}'
+      def api="/api/v1/data/bulk-operations/dataLoader/upload?nodePath="+SWEAGLE_PATH+
+        "&format="+CONFIG_FORMAT+"&allowDelete="+argDeleteData+"&onlyParent="+argOnlyParent+"&autoApprove="+argDcsApprove+
+        "&storeSnapshotResults="+argSnapshotCreate+"&validationLevel="+argSnapshotLevel+"&encoding="+argEncoding+
+        "&identifierWords="+argIdentifierWords
+      def sweagleUrl = new URL(SWEAGLE_TENANT + api)
+      println ("sweagleUrl="+sweagleUrl)
+      def sweagleCall = sweagleUrl.openConnection() as HttpURLConnection
+      sweagleCall.setRequestMethod('POST')
+      sweagleCall.setDoOutput(true)
+      sweagleCall.setRequestProperty("Accept", '*/*')
+      sweagleCall.setRequestProperty("Content-Type", 'application/json')
+      sweagleCall.setRequestProperty("Authorization", 'Bearer ' + SWEAGLE_TOKEN)
+      sweagleCall.outputStream.write(body.getBytes("UTF-8"))
+      sweagleCall.connect()
+      def response = [:]
+      println "API responseCode: "+sweagleCall.responseCode
+      if (sweagleCall.responseCode == 200 || sweagleCall.responseCode == 201) {
+        println "# Upload successfull"
+      } else {
+        response = new JsonSlurper().parseText(sweagleCall.errorStream.getText('UTF-8'))
+        println "API response: "+response
+        error("### Upload failed.")
+      }
+    }
+
+    stage('CheckConfig') {
+      echo '######################################################'
+      echo '### Ask SWEAGLE to check your configuration'
+      def errorFound = 0
+      def forIncoming = true
+      def validators = SWEAGLE_VALIDATORS.split()
+      for (i=0; i<validators.length; i++) {
+        println("# Check with validator: "+validators[i])
+        def api = "/api/v1/tenant/metadata-parser/validate?mds="+SWEAGLE_MDS+"&parser="+validators[i]+"&forIncoming="+forIncoming
+        def sweagleCall = new URL(SWEAGLE_TENANT + api).openConnection() as HttpURLConnection
+        sweagleCall.setRequestMethod('POST')
+        sweagleCall.setRequestProperty("Authorization", 'Bearer ' + SWEAGLE_TOKEN)
+        sweagleCall.setRequestProperty("Accept", '*/*')
+        sweagleCall.setRequestProperty("Content-Type", 'application/json')
+        println sweagleCall
+        sweagleCall.connect()
+
+        def response = [:]
+        println "API responseCode: "+sweagleCall.responseCode
+        if (sweagleCall.responseCode == 200 || sweagleCall.responseCode == 201) {
+          // This is ok (code 2xx)
+          response = new JsonSlurper().parseText(sweagleCall.inputStream.getText('UTF-8'))
+          println "# Validator "+validators[i]+" passed successfully"
+        } else {
+          response = new JsonSlurper().parseText(sweagleCall.errorStream.getText('UTF-8'))
+          println "response: "+response
+          if (response.error == 'NotFoundException') {
+            println "### No pending metadataset, validate last snapshot instead"
+            forIncoming = false
+            api="/api/v1/tenant/metadata-parser/validate?mds="+SWEAGLE_MDS+"&parser="+validators[i]+"&forIncoming="+forIncoming
+            sweagleCall = new URL(SWEAGLE_TENANT + api).openConnection() as HttpURLConnection
+            sweagleCall.setRequestMethod('POST')
+            sweagleCall.setRequestProperty("Authorization", 'Bearer ' + SWEAGLE_TOKEN)
+            sweagleCall.setRequestProperty("Accept", '*/*')
+            sweagleCall.setRequestProperty("Content-Type", 'application/json')
+            println sweagleCall
+            sweagleCall.connect()
+
+            int status = sweagleCall.getResponseCode();
+            println "API responseCode: "+status
+            if (status == HttpURLConnection.HTTP_OK) {
+              // This is ok (code 2xx)
+              println "# Validator "+validators[i]+" passed successfully"
+            } else {
+              // This is parser failure
+              errorFound = errorFound + 1;
+              response = new JsonSlurper().parseText(sweagleCall.errorStream.getText('UTF-8'))
+              println "# Validator "+validators[i]+" failed with error:"+response
+            }
+          } else {
+            errorFound = errorFound + 1;
+            println "# Validator "+validators[i]+" failed with error:"+response
+          }
+        }
+      }
+      if (errorFound > 0) { error("### VALIDATION FAILED. NB OF VALIDATORS FAILED: "+errorFound) }
+      else { println "### VALIDATION PASSED SUCCESSFULLY" }
+    }
+
+    stage('DownloadConfig') {
+      echo '######################################################'
+      echo '### Retrieve lastest valid configuration from SWEAGLE'
+      echo '### SWEAGLE will also fill token values, if any'
+      println "# Store the config snapshot in order to be able to retrieve it"
+      def api="/api/v1/data/include/snapshot/byname?name="+SWEAGLE_MDS+"&level=warn"
+      def sweagleCall = new URL(SWEAGLE_TENANT + api).openConnection() as HttpURLConnection
+      sweagleCall.setRequestMethod('POST')
+      sweagleCall.setRequestProperty("Authorization", 'Bearer ' + SWEAGLE_TOKEN)
+      sweagleCall.setRequestProperty("Accept", '*/*')
+      sweagleCall.setRequestProperty("Content-Type", 'application/json')
+      sweagleCall.connect()
+      def response = [:]
+      println "API responseCode: "+sweagleCall.responseCode
+      if (sweagleCall.responseCode != 200 && sweagleCall.responseCode != 201) {
+        response = new JsonSlurper().parseText(sweagleCall.errorStream.getText('UTF-8'))
+        println "API response: "+response
+        error("### SNAPSHOT FAILED")
+      }
+
+      println "# Snapshot successfull, get last config"
+      api="/api/v1/tenant/metadata-parser/parse?mds="+SWEAGLE_MDS+"&parser="+SWEAGLE_EXPORTER+ \
+      "&args="+SWEAGLE_EXPORTER_ARGS+"&format="+SWEAGLE_EXPORTER_FORMAT+"&tag="+SWEAGLE_SNAPSHOT_TAG+"&picture=false"
+      sweagleCall = new URL(SWEAGLE_TENANT + api).openConnection() as HttpURLConnection
+      sweagleCall.setRequestMethod('POST')
+      sweagleCall.setRequestProperty("Authorization", 'Bearer ' + SWEAGLE_TOKEN)
+      sweagleCall.setRequestProperty("Accept", '*/*')
+      sweagleCall.setRequestProperty("Content-Type", 'application/json')
+      sweagleCall.connect()
+      println "API responseCode: "+sweagleCall.responseCode
+      if (sweagleCall.responseCode == 200 || sweagleCall.responseCode == 201) {
+        //response = new JsonSlurper().parseText(sweagleCall.inputStream.getText('UTF-8'))
+        println "response file: "+sweagleCall.inputStream.getText('UTF-8')
+        println("### DOWNLOAD SUCCESSFULL")
+      } else {
+        response = new JsonSlurper().parseText(sweagleCall.errorStream.getText('UTF-8'))
+        println "API response: "+response
+        error("### DOWNLOAD FAILED")
+      }
+    }
+
+    stage('DownloadConfig2') {
+      echo '######################################################'
+      echo '### Retrieve lastest valid configuration from SWEAGLE'
+      echo '### SWEAGLE will also fill token values, if any'
+
+      println "# Store the config snapshot in order to be able to retrieve it"
+      def api="/api/v1/data/include/snapshot/byname?name="+SWEAGLE_MDS+"&level=warn"
+      def response = new JsonSlurper().parseText(callSweagleAPI('POST',api))
+      if (response.errorFound) {
+        error("### SNAPSHOT FAILED. ERROR IS: "+response.description)
+      }
+
+      println "# Snapshot successfull, get last config"
+      api="/api/v1/tenant/metadata-parser/parse?mds="+SWEAGLE_MDS+"&parser="+SWEAGLE_EXPORTER+ \
+      "&args="+SWEAGLE_EXPORTER_ARGS+"&format="+SWEAGLE_EXPORTER_FORMAT+"&tag="+SWEAGLE_SNAPSHOT_TAG+"&picture=false"
+      def rawResponse = callSweagleAPI('POST',api)
+      println("raw="+rawResponse)
+      response = new JsonSlurper().parseText(rawResponse)
+      if (response.errorFound) { error("### DOWNLOAD FAILED. ERROR IS: "+response.description) }
+      def index=rawResponse.indexOf("\"description\":")
+      if (index == -1) { error ("### DOWNLOAD FAILED. UNABLE TO GET CONTENT") }
+      def fileContent = rawResponse.substring(index+14,rawResponse.length()-1)
+      println " response file: "+ fileContent
+      println("### DOWNLOAD SUCCESSFULL")
+    }
+
+}
+
+// Function to call a SWEAGLE API
+def callSweagleAPI(method, api) {
+  def errorFound = false
+  def SWEAGLE_TENANT = "https://testing.sweagle.com"
+  def SWEAGLE_TOKEN = "02eb161a-d8e6-43a6-883e-affcdd0b5975"
+  def sweagleCall = new URL(SWEAGLE_TENANT + api).openConnection() as HttpURLConnection
+  sweagleCall.setRequestMethod(method)
+  sweagleCall.setRequestProperty("Accept", '*/*')
+  sweagleCall.setRequestProperty("Content-Type", 'application/json')
+  sweagleCall.setRequestProperty("Authorization", 'Bearer ' + SWEAGLE_TOKEN)
+  sweagleCall.connect()
+  def response = [:]
+  int status = sweagleCall.getResponseCode();
+  if (status == HttpURLConnection.HTTP_OK || status == 201) {
+    // This is ok (code 2xx)
+    response = sweagleCall.inputStream.getText('UTF-8')
+  } else if (status == HttpURLConnection.HTTP_MOVED_TEMP || status == HttpURLConnection.HTTP_MOVED_PERM || status == HttpURLConnection.HTTP_SEE_OTHER) {
+    // this is a redirect (code 3xx), follow it
+    errorFound = true
+    response = "Redirect to: "+sweagleCall.getHeaderField("Location");
+  } else {
+    errorFound = true
+    response = new JsonSlurper().parseText(sweagleCall.errorStream.getText('UTF-8'))
+  }
+  return '{"errorFound":'+errorFound+', "description":'+response+'}'
+}
--- jenkins/readme.md
+++ jenkins/readme.md
@@ -1,4 +1,4 @@
-# SWEAGLE Integration to Jenkins
+# SWEAGLE Integration to JENKINS
 
 DESCRIPTION
 
@@ -6,15 +6,31 @@ This folder provides examples of configuration to include SWEAGLE into a JENKINS
 SWEAGLE will become the configuration data approval gate before you build your application and deploy it to your various environments.
 SWEAGLE will also fill the tokens with the values linked to deployment context (targeted environment, release, or component, ...)
 
+Several options are available and described here to use SWEAGLE with Jenkins pipelines.
+You can
+- rely on shell scripts (available for linux or windows),
+- use official SWEAGLE Jenkins plug-In,
+- or use direct REST calls in Jenkins groovy script.
+
+
+JENKINS SWEAGLE OFFICIAL PLUG-IN
+
+Jenkins official plug-in is available from here:
+https://plugins.jenkins.io/sweagle
+
+
 PRE-REQUISITES
 
-You should use the scripts provided here with the scripts provided under the linux or windows directory.
+For jenkinsfile-shell-(declarative or scripted), you should use the scripts provided here with the scripts provided under the linux or windows directory.
+Note: Script "uploadDirToSweagle" requires no space in the name of your jenkins project.
 
-Script "uploadDirToSweagle" requires no space in the name of your jenkins project.
+For jenkinsfile-plugin, you should first install SWEAGLE official plugin on your Jenkins.
+
 
 INSTALLATION
 
-1. Put all linux or windows SWEAGLE shell scripts into one folder of your jenkins job workspace (for example "/sweagle_scripts") or your git repository (if you are using jenkins pipeline with git plugins)
+For jenkinsfile-shell-declarative or jenkinsfiles-shell-scripted
+1. Put all linux or windows SWEAGLE shell scripts into one folder of your jenkins job workspace (for example in "/sweagle_scripts") or your git repository (if you are using jenkins pipeline with git plugins)
 2. Open the "sweagle.env" script and put your values for SWEAGLE tenant, API token, and scripts path (for ex: "./sweagle_scripts")
 3. In Jenkins:
     - if you use classic steps/jobs approach, configure your project and add Shell steps in order to use SWEAGLE scripts. Recommended approach is:
@@ -30,11 +46,15 @@ That's all !
 
 CONTENT
 
-/jenkinsfile-declarative         : Sample JENKINS declarative pipeline file, supported only since Jenkins v2.15
+/jenkinsfile-directAPI-groovy         : Sample JENKINS pipeline file using groovy script to call SWEAGLE APIs directly from pipeline script (no prerequisites)
 
-/jenkinsfile-scripted         : Sample JENKINS scripted pipeline file (groovy style), supported since Jenkins 2.0
+/jenkinsfile-plugin         : Sample JENKINS pipeline file using SWEAGLE plugin
 
-In both scripts, stages are the same:
+/jenkinsfile-shell-declarative         : Sample JENKINS declarative pipeline file using shell scripts, supported only since Jenkins v2.15
+
+/jenkinsfile-shell-scripted         : Sample JENKINS scripted pipeline file using shell script (groovy style), supported since Jenkins 2.0
+
+In all scripts, stages are similar:
 - Stage "uploadConfig" is used to upload config files to SWEAGLE
     - Call input:
         - CONFIG_DIR = Config file or directory containing all files to upload to SWEAGLE
