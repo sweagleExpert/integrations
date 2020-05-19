0b88316a6d9ccbff5e2c8900abea4e32177a7758
Dimitris <dimitris@sweagle.com>
2020-05-19 17:48:52 +0200

    add lookup plugin

---
+++ ansible/plugins/lookup/sweagle.py
@@ -0,0 +1,140 @@
+# python 3 headers, required if submitting to Ansible
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = """
+        lookup: sweagle
+        author: Dimitris Finas <dimitris@sweagle.com>
+        version_added: "0.1"
+        short_description: read content from SWEAGLE
+        description:
+            - This lookup returns specified configurations variables & values from a SWEAGLE platform.
+        options:
+          _terms:
+            description: CDS = Configuration Data Set to export or search for key/value
+            required: true
+          sweagle_args:
+            description: optional args to use for exporter selected
+            type: string
+          sweagle_parser:
+            description: Exporter used
+            type: string
+            default: "all"
+          sweagle_tag:
+            description: Tag of the configuration to retrieve, default is empty meaning latest release
+            type: string
+            default: ""
+          sweagle_tenant:
+            description: Tenant to connect to
+            type: string
+            default: "https://testing.sweagle.com"
+          sweagle_token:
+            description: API Token used to authenticate
+            type: string
+          use_proxy:
+            description: Flag to control if the lookup will observe HTTP proxy environment variables when present.
+            type: boolean
+            default: False
+        notes:
+          - all parameters are optional, they will be replaced by default values in code if not provided.
+"""
+
+EXAMPLES = """
+- name: return a specific node in a configuration and assing it to a var
+- hosts: all
+  vars:
+     contents: "{{ lookup('sweagle', 'samples-test-dev', sweagle_args='dev', sweagle_parser='returnDataForNode') }}"
+  tasks:
+     - debug:
+         msg: the value of SWEAGLE configuration is {{ contents }}
+
+- name: return a full ConfigDataSet (CDS)
+  debug: msg="{{ lookup('sweagle', 'samples-test-dev') }}"
+
+- name: search value of specific key
+  debug: msg="{{ lookup('sweagle', 'samples-test-dev', sweagle_args='resource.dev_stack.maxPoolSize', sweagle_parser='returnValueforKey') }}"
+"""
+
+RETURN = """
+  _list:
+    description: list of configurations calculated by SWEAGLE in json format
+"""
+
+from ansible.errors import AnsibleError
+#from ansible.module_utils.six.moves.urllib.error import HTTPError, URLError
+from ansible.module_utils._text import to_text, to_native
+#from ansible.module_utils.urls import open_url, ConnectionError, SSLValidationError
+from ansible.plugins.lookup import LookupBase
+from ansible.utils.display import Display
+import os
+import requests
+
+display = Display()
+
+
+class LookupModule(LookupBase):
+
+    def run(self, terms, variables=None, **kwargs):
+
+        self.set_options(direct=kwargs)
+
+        # Manage input options with default values
+        if self.get_option('sweagle_args'):
+            sweagle_args = self.get_option('sweagle_args')
+        else:
+            sweagle_args = ""
+        if self.get_option('sweagle_parser'):
+            sweagle_parser = self.get_option('sweagle_parser')
+        else:
+            sweagle_parser = "all"
+        if self.get_option('sweagle_tag'):
+            sweagle_tag = self.get_option('sweagle_tag')
+        else:
+            sweagle_tag = ""
+        if self.get_option('sweagle_tenant'):
+            sweagle_tenant = self.get_option('sweagle_tenant')
+        else:
+            sweagle_tenant = "https://testing.sweagle.com"
+        if self.get_option('sweagle_token'):
+            sweagle_token = self.get_option('sweagle_token')
+        else:
+            sweagle_token = "104e6b08-9bbf-4b66-a06a-30b7996e5d2f"
+
+        display.vvvv("[sweagle_lookup]: Lookup with exporter ("+sweagle_parser+") and args ("+sweagle_args+") from SWEAGLE tenant "+sweagle_tenant)
+
+        headers = { 'Authorization': 'Bearer ' + sweagle_token, 'Accept': 'application/json', 'Content-Type': 'application/json' }
+
+        # For future use, add here code to add proxies if required
+        if not self.get_option('use_proxy'):
+            proxies = { "http": None, "https": None }
+            # This is to disable proxy for all hosts as it generates errors on local MacOS at least
+            os.environ['no_proxy'] = '*'
+
+        ret = []
+        for term in terms:
+            display.vvvv("[sweagle_lookup]: Will export Sweagle data from CDS=%s" % term)
+            try:
+                url = sweagle_tenant+'/api/v1/tenant/metadata-parser/parse?mds=' + term + \
+                    '&parser=' + sweagle_parser + \
+                    '&format=json' + \
+                    '&args=' + sweagle_args + \
+                    '&tag=' + sweagle_tag
+                display.vvvv("[sweagle_lookup]: url= %s" % url)
+
+                response = requests.post(url, headers=headers, proxies=proxies)
+                display.vvvv("[sweagle_lookup]: response code="+ str(response.status_code))
+                display.vvvv("[sweagle_lookup]: response headers="+ str(response.headers))
+                display.vvvv("[sweagle_lookup]: response content="+ str(response.text))
+                display.vvvv("[sweagle_lookup]: proxy= %s" % self.get_option('use_proxy'))
+
+            except HTTPError as e:
+                raise AnsibleError("Received HTTP error for %s : %s" % (term, to_native(e)))
+            except URLError as e:
+                raise AnsibleError("Failed lookup url for %s : %s" % (term, to_native(e)))
+            except SSLValidationError as e:
+                raise AnsibleError("Error validating the server's certificate for %s: %s" % (term, to_native(e)))
+            except ConnectionError as e:
+                raise AnsibleError("Error connecting to %s: %s" % (term, to_native(e)))
+
+            ret.append(response.text)
+        return ret
--- ansible/readme.md
+++ ansible/readme.md
@@ -1,31 +1,49 @@
 # SWEAGLE Integration to ANSIBLE
 
-DESCRIPTION
+## DESCRIPTION
 
 This folder provides examples of script to integrate ANSIBLE with SWEAGLE
 
 Use case are multiples:
-- SWEAGLE as configuration control tower
+1. SWEAGLE as configuration control tower
     - use SWEAGLE as a control tower for any configuration deployed with ansible. SWEAGLE will validate a configuration before deploying it
-    - For this use case, look at `/roles` folder and examples below
+    - for this use case, look at `/roles` folder and examples below
 
-- SWEAGLE as vault for Ansible secrets
-  - For this use case, look at `/plugins` folder
+2. SWEAGLE as storage for Ansible vars
+  - remove the pain from manually managing Ansible vars files
+  - have more secure storage with SWEAGLE fine-grained RBAC
+  - benefit from validation rules
+  - benefit from automatic tokens calculation based on target deployed environment
+  - for this use case, look at `/plugins` folder
 
-- ANSIBLE Facts as source of hosts information
+3. SWEAGLE as vault for Ansible secrets
+  - this is similar to use case 2 above with use of specific SWEAGLE ConfigDataSet called `vault`
+  - for this use case, look at `/plugins` folder
+
+4. ANSIBLE Facts as source of SWEAGLE hosts information
   - push ANSIBLE Facts into SWEAGLE to run multiple validation rules on them or reuse them for cross validation with other configurations
   - discover & retrieve hosts configuration regularly to allow SWEAGLE to control configuration consistency, for example checking the patch level is correct or a package is present before deploying a new release requiring it.
-  - For this use case, look at playbook file `uploadFacts.yml`, that relies on SWEAGLE `/roles` provided here also
+  - for this use case, look at playbook file `uploadFacts.yml`, that relies on SWEAGLE `/roles` provided here also
 
 The ansible roles provided here works both with API or CLI (if commands exists) depending on value of parameter "use_cli" available in ./group_vars/all.yml
 
 
-# PRE-REQUISITES
+## PRE-REQUISITES
 
 ANSIBLE v2.5 is required for these scripts to work to allow support of loops
 
 
-# CONTENT & EXAMPLES
+## INSTALLATION
+
+- For all use cases, you can benefit from SWEAGLE most used APIs to upload, validate, snapshot or export your configuration data by using roles provided in `/roles` folder
+
+- For uses cases 2 & 3, you can install SWEAGLE specific plugins directly next to your plays in a `/lookup_plugins` folder or by putting in ansible default plugins folders, like `~/.ansible/plugins/lookup:/usr/share/ansible/plugins/lookup`
+
+- More details on custom plugins installation are available here:
+https://docs.ansible.com/ansible/latest/plugins/lookup.html#enabling-lookup-plugins
+
+
+## CONTENT & EXAMPLES
 
 Each SWEAGLE API/CLI command is available under a specific `/roles/sweagle` folder
 
